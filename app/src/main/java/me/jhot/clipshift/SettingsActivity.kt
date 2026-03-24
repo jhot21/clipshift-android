b547e227eb6ae718f630ed4ae897c2cfa3824a25
jhot <code@jhot.me>
2026-03-23 20:11:34 -0600

    fix: remove unused ScrollView import from SettingsActivity

--- app/src/main/java/me/jhot/clipshift/SettingsActivity.kt
+++ app/src/main/java/me/jhot/clipshift/SettingsActivity.kt
@@ -7,7 +7,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.ScrollView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
